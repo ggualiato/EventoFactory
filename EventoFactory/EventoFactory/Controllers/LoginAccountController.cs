7e1ec1a3b6d13c141db4ae72d422b1c3505ccfb6
Igor Ramos Rocha Santos <RE035175@resource.com.br>
2017-08-10 15:11:02 -0300

    Conectando com o Banco

---
+++ EventoFactory/EventoFactory/Controllers/LoginAccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventoFactory.Controllers
+{
+    public class LoginAccountController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
