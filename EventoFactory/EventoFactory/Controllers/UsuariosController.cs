f72dc7200bdd5c04f779b1ca19c50ef0e7657299
Felipe do Amaral Altenfelder Silva <re035143@resource.com.br>
2017-08-14 18:13:26 -0300

    Cadastrar Usuario-Arrumar validacao

---
+++ EventoFactory/EventoFactory/Controllers/UsuariosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventoFactory.Models;
+using EventoFactory.DAO;
+
+namespace EventoFactory.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Usuarios usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                if (usuarioDAO.CadastrarUsuario(usuario))
+                {
+                    ViewBag.Mensagem = "DEU CERTO";
+                }
+                else
+                {
+                    ViewBag.Mensagem = "EEERRRRROUUUUUUU BIXO";
+                }
+            }            
+
+            return View();
+        }
+
+    }
+}
---
+++ EventoFactory/EventoFactory/DAO/UsuarioDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventoFactory.Models;
+
+namespace EventoFactory.DAO
+{
+    public class UsuarioDAO
+    {
+        ServidorEventoEntities1 db = new ServidorEventoEntities1();
+
+        public Boolean CadastrarUsuario(Usuarios usuario)
+        {
+            try
+            {
+                db.Usuarios.Add(usuario);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+    }
+}
