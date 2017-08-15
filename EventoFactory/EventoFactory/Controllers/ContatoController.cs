56e0cd0bcf584067f472c3b20138860e70f8d1c6
Matheus Dider Fernandes de Lima <re035164@resource.com.br>
2017-08-15 15:23:52 -0300

    Criei 2 controllers novas: Sobre e contato

---
+++ EventoFactory/EventoFactory/Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventoFactory.Controllers
+{
+    public class ContatoController : Controller
+    {
+        //
+        // GET: /Contato/
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EventoFactory/EventoFactory/Controllers/SobreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventoFactory.Controllers
+{
+    public class SobreController : Controller
+    {
+        //
+        // GET: /Sobre/
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+    }
+}
