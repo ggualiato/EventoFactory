6942e64c3e0ca7e22683e3ab3afde688aa204a25
Matheus Dider Fernandes de Lima <re035164@resource.com.br>
2017-08-15 14:31:11 -0300

    Atualizacao 15/08

---
+++ EventoFactory/EventoFactory/Controllers/HistoricoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventoFactory.Controllers
+{
+    public class HistoricoController : Controller
+    {
+        //
+        // GET: /Historico/
+
+        public ActionResult Historico()
+        {
+            return View();
+        }
+
+    }
+}
